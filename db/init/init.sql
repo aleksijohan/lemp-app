c97f684b0304441015c4e518a0a783cb87fa2d02
aleksijohan <147402027+aleksijohan@users.noreply.github.com>
2025-12-11 18:19:18 +0200

    Delete db directory

--- db/init/init.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE IF NOT EXISTS appdb;
-CREATE USER IF NOT EXISTS 'appuser'@'%' IDENTIFIED BY 'changeme';
-GRANT ALL PRIVILEGES ON appdb.* TO 'appuser'@'%';
-FLUSH PRIVILEGES;
-
--- For debugging purposes, you can use the
